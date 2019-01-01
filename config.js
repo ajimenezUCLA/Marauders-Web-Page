c8d4a97c53e482a6fe0edda71d0e76739ee83e3d
ajimenezUCLA <angelvj90@gmail.com>
2018-12-31 19:36:35 -0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+var config = {
+  firebase: 'scoots-da97f',
+  mapbox: {
+      accessToken: 'pk.eyJ1IjoiYW5nZWx2ajkwIiwiYSI6ImNqYXJsd2QybTRqNjQyd3F1NzZkZmluOGMifQ.Ts4oIqsSV7Lu6H9DSZZ0iw',
+      mapId: 'mattketmo.locpi8o0',
+  },
+  analytics: 'UA-23657373-6',
+}
